0160eed61d66e7e4171c75d0671c9a79d70df522
unknown <moe_aoki@office.dwango.co.jp>
2021-04-15 10:59:02 +0900

    call back

--- index.js
+++ index.js
@@ -1,95 +1,26 @@
 'use strict';
-// { name: タスクの文字列, state: 完了しているかどうかの真偽値 }
-let tasks = [];
+const assert = require('assert');
+
+// テストの前に永続化されているファイルを消す
 const fs = require('fs');
-const fileName = './tasks.json';
+fs.unlink('./tasks.json', err => {
+  const todo = require('./index.js');
 
-// 同期的にファイルから復元
-try {
-  const data = fs.readFileSync(fileName, 'utf8');
-  tasks = JSON.parse(data);
-} catch (ignore) {
-  console.log(fileName + 'から復元できませんでした');
-}
+  // add と list のテスト
+  todo.add('ノートを買う');
+  todo.add('鉛筆を買う');
+  assert.deepStrictEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
 
-/**
- * タスクをファイルに保存する
- */
-function saveTasks() {
-  fs.writeFileSync(fileName, JSON.stringify(tasks), 'utf8');
-}
+  // done と donelist のテスト
+  todo.done('鉛筆を買う');
+  assert.deepStrictEqual(todo.list(), ['ノートを買う']);
+  assert.deepStrictEqual(todo.donelist(), ['鉛筆を買う']);
 
-/**
- * TODOを追加する
- * @param {string} task
- */
-function add(task) {
-  tasks.push({ name: task, state: false });
-  saveTasks();
-}
+  // del のテスト
+  todo.del('ノートを買う');
+  todo.del('鉛筆を買う');
+  assert.deepStrictEqual(todo.list(), []);
+  assert.deepStrictEqual(todo.donelist(), []);
 
-/**
- * タスクと完了したかどうかが含まれる配列を受け取り、完了したかを返す
- * @param {array} taskAndIsDonePair
- * @return {boolean} 完了したかどうか
- */
-function isDone(taskAndIsDonePair) {
-  return taskAndIsDonePair.state;
-}
-
-/**
- * タスクと完了したかどうかが含まれる配列を受け取り、完了していないかを返す
- * @param {array} taskAndIsDonePair
- * @return {boolean} 完了していないかどうか
- */
-function isNotDone(taskAndIsDonePair) {
-  return !isDone(taskAndIsDonePair);
-}
-
-/**
- * TODO一覧の配列を取得する
- * @return {array}
- */
-function list() {
-  return tasks.filter(isNotDone).map(t => t.name);
-}
-
-/**
- * TODOを完了状態にする
- * @param {string} task
- */
-function done(task) {
-  const indexFound = tasks.findIndex(t => t.name === task);
-  if (indexFound !== -1) {
-    tasks[indexFound].state = true;
-    saveTasks();
-  }
-}
-
-/**
- * 完了済みのタスクの一覧の配列を取得する
- * @return {array}
- */
-function donelist() {
-  return tasks.filter(isDone).map(t => t.name);
-}
-
-/**
- * 項目を削除する
- * @param {string} task
- */
-function del(task) {
-  const indexFound = tasks.findIndex(t => t.name === task);
-  if (indexFound !== -1) {
-    tasks.splice(indexFound, 1);
-    saveTasks();
-  }
-}
-
-module.exports = {
-  add,
-  list,
-  done,
-  donelist,
-  del
-};
+  console.log('テストが正常に完了しました');
+});
