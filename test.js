012888fe606c878cf0574dbf38199ab60a3cb6b3
BlueZhiaar <tightblackmachine@gmail.com>
2021-09-23 10:20:55 +0900

    テストの際既にあるファイルを削除するようにした

--- test.js
+++ test.js
@@ -1,6 +1,32 @@
 'use strict';
 const todo = require('./index.js');
 const assert = require('assert');
+const fs = require('fs');
+
+//tasks.jsonの削除
+fs.unlink('./tasks.json', err => {
+    // add と list のテスト
+todo.add('ノートを買う');
+todo.add('鉛筆を買う');
+assert.deepStrictEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+
+// done と donelist のテスト
+todo.done('鉛筆を買う');
+assert.deepStrictEqual(todo.list(), ['ノートを買う']);
+assert.deepStrictEqual(todo.donelist(), ['鉛筆を買う']);
+
+// del のテスト
+todo.del('ノートを買う');
+todo.del('鉛筆を買う');
+assert.deepStrictEqual(todo.list(), []);
+assert.deepStrictEqual(todo.donelist(), []);
+
+console.log('テストが正常に完了しました');
+
+
+});
+
+/*
 
 // add と list のテスト
 todo.add('ノートを買う');
@@ -19,3 +45,5 @@ assert.deepStrictEqual(todo.list(), []);
 assert.deepStrictEqual(todo.donelist(), []);
 
 console.log('テストが正常に完了しました');
+
+*/
