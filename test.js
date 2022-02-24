1e7967bcb04dab40fa6772a83a0dc3ff30e8bfd6
nekonabesan <sheepdogs_2@hotmail.com>
2022-02-24 18:42:14 +0900

    入門コースの3章11節の練習 (ISC License)ソース修正

--- test.js
+++ test.js
@@ -1,21 +1,31 @@
 'use strict';
+const fs = require('fs');
 const todo = require('./index.js');
 const assert = require('assert');
 
-// add と list のテスト
-todo.add('ノートを買う');
-todo.add('鉛筆を買う');
-assert.deepStrictEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+fs.unlink('./tasks.json', err => {
+  // ファイルが削除されなかった場合
+  if (err) {
+    // ERRログを出力して処理を抜ける
+    console.error(err);
+    return false;
+  }
 
-// done と donelist のテスト
-todo.done('鉛筆を買う');
-assert.deepStrictEqual(todo.list(), ['ノートを買う']);
-assert.deepStrictEqual(todo.donelist(), ['鉛筆を買う']);
+  // add と list のテスト
+  todo.add('ノートを買う');
+  todo.add('鉛筆を買う');
+  assert.deepStrictEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
 
-// del のテスト
-todo.del('ノートを買う');
-todo.del('鉛筆を買う');
-assert.deepStrictEqual(todo.list(), []);
-assert.deepStrictEqual(todo.donelist(), []);
+  // done と donelist のテスト
+  todo.done('鉛筆を買う');
+  assert.deepStrictEqual(todo.list(), ['ノートを買う']);
+  assert.deepStrictEqual(todo.donelist(), ['鉛筆を買う']);
 
-console.log('テストが正常に完了しました');
+  // del のテスト
+  todo.del('ノートを買う');
+  todo.del('鉛筆を買う');
+  assert.deepStrictEqual(todo.list(), []);
+  assert.deepStrictEqual(todo.donelist(), []);
+
+  console.log('テストが正常に完了しました');
+});
